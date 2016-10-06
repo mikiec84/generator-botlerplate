723ebde9b58395f97e881b4c6b8f1ee4f0bb9651
François Triquet <francois.triquet@recast.ai>
2016-10-06 13:24:02 +0200

    start microsoft bot connector template

--- generators/app/templates/_serverMicrosoft.js
+++ generators/app/templates/_serverMicrosoft.js
@@ -1,25 +1,52 @@
-import _ from 'lodash'
-import { Bot, Action }from 'recastai-botlerplate'
+import express from 'express'
+import builder from 'botbuilder' <% if (sources) { %>
+import Bot from './core/bot' <% } else { %>
+import { Bot } from 'botlerplate' <% } %>
+import Greetings from './actions/greetings'
 import config from './config'
 
-const bot = new Bot()
+const recastToken = ''
+
+// RECAST BOT INSTANCE
+// A language can be provided in the constructor
+const myBot = new Bot({
+  token: recastToken || process.env.TOKEN || process.argv[2],
+  notIntent: {
+    en: ['Sorry but I don\'t understand.'],
+    fr: ['Desole mais je ne comprends pas.'],
+  },
+  // language: 'en',
+})
+
+myBot.registerActions([Greetings])
+
 <% if (mongo) { %>
+// DATABASE INITIALIZATION
 bot.useMongo(config.database)
 <% } %>
 
-/* ***************** *\
- *     Actions
-\* ***************** */
-bot.registerAction(class Greetings extends Action {
-  constructor(conversationData) {
-    super()
-    this.answers = {
-      en: ['Hey', 'Hello!'],
-      fr: ['Salut', 'Bonjour!'],
-    }
-  }
+// CONNECTION TO MICROSOFT BOT
+const connector = new builder.ChatConnector({
+  appId: config.microsoft.appId,
+  appPassword: config.microsoft.secret,
 })
-<% if (microsoft) { %>
-bot.useMicrosoftBotConnector(config.microsoft)
-server.post('/api/messages', bot.listen())
-<% } %>
+const microsoftBot = new builder.UniversalBot(connector)
+
+microsoftBot.dialog('/', session => {
+  // User input
+  const text = session.message.text
+
+  // User's conversation unique id
+  // It will be used by to identify each conversation with a user
+  const conversationId = session.message.address.conversation.id
+
+  // Token and landuage can laso be passed as arguments to reply
+  myBot.reply(text, conversationId, {}).then(replies => {
+    replies.forEach(reply => {
+      session.send(reply)
+    })
+  }).catch(err => {
+    console.log(`An error occured: ${err}`)
+    session.send('Oops, I got a problem.')
+  })
+})
