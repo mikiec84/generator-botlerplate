e4c684bb337fa5f4298e7f8eada4c35338a5b88e
Bruno <bruno.gantelmi@recast.ai>
2016-10-06 13:28:05 +0200

    [UPD] update server slack

--- generators/app/templates/_serverSlack.js
+++ generators/app/templates/_serverSlack.js
@@ -2,8 +2,18 @@
 import Bot from 'botlerplate' <% } %>
    <% if (sources === 'sources') { %>
 import Bot from './core/bot' <% } %>
-
 import config from '../config'
+import slack from '@slack/client'
+
+const SlackClient = slack.RtmClient
+const slackEvent = slack.RTM_EVENTS
+const rtm = new SlackClient(config.slack.token, { logLevel: 'false' })
+rtm.start()
+
+rtm.on(slackEvent.MESSAGE, (message) => {
+  const user = rtm.dataStore.getUserById(message.user)
+  const dm = rtm.dataStore.getDMByName(user.name).id
+})
 
 const recastToken = '' || process.env.TOKEN || process.argv[2]
 
@@ -20,8 +30,8 @@ const myBot = new Bot({
 
 
 /** boucle principale (slack/facebook/whatever) **/
-myBot.reply(text, IDDeLaConversation).then(replies => {
+myBot.reply(text, IdConversation).then(replies => {
   responses.forEach(reply => {
-    envoyerLaResponseAuChannel(reply)
+    rtm.sendMessage(reply)
   })
 })
